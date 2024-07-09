32289e77f9fc5ff48b0633e8241fb69310140bf6
Tdun2005 <lqd9a1tungduongphamnguyen@gmail.com>
2024-07-09 14:22:13 +0700

    Create bai58.cs

---
+++ bai58.cs
@@ -0,0 +1,11 @@
+1. Khai báo với kiểu float trực tiếp
+Khai báo biến với kiểu dữ liệu float và sử dụng hậu tố f để chỉ rõ rằng giá trị 3.0 là kiểu float.
+float myFloat1 = 3.0f;
+2. Khai báo với từ khóa var và hậu tố f
+Sử dụng từ khóa var để khai báo biến, và giá trị khởi tạo với hậu tố f để trình biên dịch hiểu đây là kiểu float.
+
+var myFloat2 = 3.0f;
+3. Khai báo với kiểu float và khởi tạo từ một biểu thức float
+Khai báo biến với kiểu float và khởi tạo bằng cách chuyển đổi từ một giá trị double sang float bằng cách sử dụng một biểu thức kiểu float.
+
+float myFloat3 = (float)3.0;
